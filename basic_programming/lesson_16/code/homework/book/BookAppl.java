826b1c6535ec362fb5384e93ecbdf062a2b89ede
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-03-26 17:44:27 +0100

    lesson update

---
+++ basic_programming/lesson_16/code/homework/book/BookAppl.java
@@ -0,0 +1,13 @@
+package homework.book;
+
+import homework.book.model.Book;
+
+public class BookAppl {
+    public static void main(String[] args) {
+
+        Book book = new Book("War and peace", "Tolstoy", 1980, 1000L);
+        book.display();
+        System.out.println("----------------------------------------");
+
+    }
+}

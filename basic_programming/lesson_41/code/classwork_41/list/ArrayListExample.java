8021e4c97b038df4dff7c9d21904b5636920b7fb
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-06-26 22:06:07 +0200

    Lesson update

---
+++ basic_programming/lesson_41/code/classwork_41/list/ArrayListExample.java
@@ -0,0 +1,14 @@
+package classwork_41.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        List<Integer> intNumbers = new ArrayList<Integer>(10);
+
+
+
+    }
+}
